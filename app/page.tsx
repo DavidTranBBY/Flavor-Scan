c8ba5907c9e8fde7587985a5fce5ba6dcaa271e7
AnNewDev <trantuananbussiness@gmail.com>
2026-03-25 14:10:54 -0500

    Install GSAP, quick intro for landing page.

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,13 @@
-import { pixel } from "@/lib/fonts"
+import IntroDoors from "@/components/ui/IntroDoors"
 
 export default function HomePage() {
   return (
-    <main className="mx-auto max-w-6xl px-4 py-10">
-      <div className="flex items-center justify-center flex-col h-screen">
-        <section className="space-y-4">
-          <h1 className={`text-6xl ${pixel.className}`}>MAIN</h1>
-          <p> test default content </p>
-        </section>
-      </div>
+    <main>
+      <IntroDoors />
+
+      <section className="min-h-screen flex items-center justify-center">
+        <h1 className="text-4xl font-bold">Hello World</h1>
+      </section>
     </main>
   )
 }
